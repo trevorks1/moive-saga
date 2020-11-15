927f381345617b233189e8027802deeab34ac54b
Trevor Sansalone <trevorksansalone@gmail.com>
2020-11-15 11:24:50 -0600

    reentering my app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import AddMoviePage from '../../pages/AddMoviesPage/AddMoviePage';
+import DetailsPage from '../../pages/DetailsPage/DetailsPage';
+import HomePage from '../../pages/HomePage/HomePage';
 import './App.css';
-import HomePage from '../pages/HomePage/HomePage';
-
-// Use of redux
-import { connect } from 'react-redux';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
-      <div className="App">
-        <h1>Movies!</h1>
-        <Router>
+      <Router>
+        <div className="App">
+          <h1>Movies!</h1>
+          <nav>
+            <h3>
+              <Link to="/">Home Page</Link>
+            </h3>
+            <h3>
+              <Link to="/add">Add New Movie</Link>
+            </h3>
+          </nav>
           <Route exact path="/" component={HomePage} />
-        </Router>
-        <p>Empty Page</p>
-      </div>
+          <Route exact path="/details/:id" component={DetailsPage} />
+          <Route exact path="/add" component={AddMoviePage} />
+        </div>
+      </Router>
     );
   }
 }
 
-// Map to State Props and reduxState
-const mapStateToProps = (reduxState) => reduxState;
-
-export default connect(mapStateToProps)(App);
+export default App;
